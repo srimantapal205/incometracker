e7d335f2faab6eca4c4e9db73c335b94c392369c
Srimanta Pal <srimantapal205@hotmail.com>
2022-09-13 23:15:12 +0530

    Build an Income Tracker  new project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <HeaderComounent />
+
+  <router-view />
 </template>
+<script>
+import HeaderComounent from "@/components/HeaderComounent.vue";
+export default {
+  name: "app-root",
+  setup() {
+    return {
+      HeaderComounent,
+    };
+  },
+};
+</script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
